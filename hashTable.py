ccac98948cc9aae831b048cebc416bf462639fa7
Matt Geiger <Mail@MattGeiger.com>
2016-03-02 16:25:22 -0500

    Updated hashTable
    Now supports collisions & re-writing

--- hashTable.py
+++ hashTable.py
@@ -12,9 +12,25 @@ class hashTable(object):
     def add(self, key, value):
         dataHash = self.hash(key)
         if self.table[dataHash] == None:
-            self.table[dataHash] = value
+            self.table[dataHash] = [[key, value]]
         else:
-            print "Collision at "+str(dataHash)
+            # posible collision
+            for entry in self.table[dataHash]:
+                if entry[0] == key:
+                    # just a re-write
+                    entry[1] = value
+                    break
+            else:
+                # collision
+                self.table[dataHash].append([key, value])
 
     def get(self, key):
-        return self.table[self.hash(key)]
+        entry = self.table[self.hash(key)]
+        if entry == None:
+            return None
+        elif len(entry) < 2:
+            return entry[0][1]
+        else:
+            for i in entry:
+                if i[0] == key:
+                    return i[1]
